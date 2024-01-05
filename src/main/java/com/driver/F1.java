7b9d62b8636e0a4dfc4aeba395678fc515897d1a
Piyush Verma <piyushvrm265@gmail.com>
2024-01-05 13:16:58 +0530

    ---

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -50,8 +50,8 @@ public class F1 extends Car {
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, getCurrentDirection());
+//        }
     }
 }
