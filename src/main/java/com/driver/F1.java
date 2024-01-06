49c9f5893703265e4fc857a8700320d74c75f7ad
Piyush Verma <piyushvrm265@gmail.com>
2024-01-06 22:53:09 +0530

    Testing F1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,14 +4,11 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2, 6, isManual, "Super Car", 1);
-
+        super(name, 4, 2, 6, true, "Super Car", 1);
     }
 
     public void accelerate(int rate){
-        //set the value of new speed by using currentSpeed and rate
-        int newSpeed = getCurrentSpeed() + rate;
-
+        int newSpeed = getCurrentSpeed() + rate;; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -21,8 +18,6 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
-
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
@@ -53,6 +48,7 @@ public class F1 extends Car {
         else{
             changeGear(6);
         }
+        //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
