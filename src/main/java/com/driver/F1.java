11c80f2c31a4af0a5bbc1caeb847b17b0268c1fb
Piyush Verma <piyushvrm265@gmail.com>
2024-01-06 23:09:16 +0530

    Testing

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,14 +1,70 @@
 package com.driver;
 
+
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2, 6, true, "Super Car", 1);
+        super(name, 4, 2, 6, isManual, "Super Car", 1);
+
     }
 
+//    public void accelerate(int rate){
+//        //set the value of new speed by using currentSpeed and rate
+//        int newSpeed = getCurrentSpeed() + rate;
+//
+//        /**
+//         * speed 0: gear 1
+//         * speed 1-50: gear 1
+//         * speed 51-100: gear 2
+//         * speed 101-150: gear 3
+//         * speed 151-200: gear 4
+//         * speed 201-250: gear 5
+//         * speed more than 250: gear 6
+//         */
+//
+//
+//
+//        if(newSpeed == 0) {
+//            //Stop the car, set gear as 1
+//            stop();
+//            changeGear(1);
+//        }
+//        //for all other cases, change the gear accordingly
+//        else if (newSpeed >= 1 && newSpeed <= 50) {
+//            changeGear(1);
+//
+//        }
+//        else if (newSpeed >= 51 && newSpeed <= 100) {
+//            changeGear(2);
+//
+//        }
+//        else if (newSpeed >= 101 && newSpeed <= 150) {
+//            changeGear(3);
+//
+//        }
+//        else if (newSpeed >= 151 && newSpeed <= 200) {
+//            changeGear(4);
+//
+//        }
+//        else if (newSpeed >= 201 && newSpeed <= 250) {
+//            changeGear(5);
+//
+//        }
+//        else{
+//            changeGear(6);
+//        }
+//
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, getCurrentDirection());
+//        }
+//    }
+
+
     public void accelerate(int rate){
-        int newSpeed = getCurrentSpeed() + rate;; //set the value of new speed by using currentSpeed and rate
+        //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed() + rate;
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,6 +74,8 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+
+
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
@@ -48,7 +106,6 @@ public class F1 extends Car {
         else{
             changeGear(6);
         }
-        //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
